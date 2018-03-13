fd971f2c757cbe453a74c7b6c69fbcb35116890b
zkhCreator <zkh90644@gmail.com>
2018-03-13 23:24:26 +0800

    remove groupview

--- TimeCapsule/Editor/Views/DatePicker/TCDatePickerView.swift
+++ TimeCapsule/Editor/Views/DatePicker/TCDatePickerView.swift
@@ -10,36 +10,85 @@ import UIKit
 
 class TCDatePickerView: UIView {
     
-    var dateGroupView = TCDatePickerGroupView()
+    var currentDate:Int
+    var selectedDate:Int
+    var buttonArray = [UIButton]()
+    var selectedButton:UIButton?
+    
+    var firstDayWeekday = TCWeekday.Sunday
+    
     var selectedLabel:UILabel?
     
     override init(frame: CGRect) {
+        currentDate = 1
+        selectedDate = 1
         super.init(frame: frame)
-
-        addSubview(dateGroupView)
         setupView()
+        
+        for i in 0 ..< 31 {
+            let button = UIButton.init(type: .custom)
+            button.tag = i
+            button.setTitle("\(i + 1)", for: .normal)
+            button.addTarget(self, action: #selector(click(button:)), for: .touchUpInside)
+            button.titleLabel?.textAlignment = .center
+            button.setTitleColor(UIColor.white, for: .selected)
+            button.setTitleColor(UIColor.black, for: .normal)
+            buttonArray.append(button)
+            addSubview(button)
+            
+        }
     }
     
     func setupView() {
-        dateGroupView.frame = CGRect(x: 16, y: 0, width: self.bounds.size.width - 16 * 2, height: self.bounds.height)
+        var currentDayOffset = firstDayWeekday.rawValue - 1
+        let offsetX:CGFloat = 0
+        let viewWidth = self.bounds.width - CGFloat(offsetX * 2)
+        let singleWidth = viewWidth / CGFloat(7)
+        
+        for label in buttonArray {
+            let line = (currentDayOffset / 7)
+            label.frame = CGRect(x: offsetX + singleWidth * CGFloat(currentDayOffset % 7), y: CGFloat(line) * CGFloat(singleWidth), width: singleWidth, height: singleWidth)
+            
+            currentDayOffset = currentDayOffset + 1
+        }
     }
     
     func update(with showModel:TCEventShowModel) {
-        self.dateGroupView.update(with: showModel)
-        self.dateGroupView.setNeedsLayout()
+        firstDayWeekday = showModel.manager.firstDayWeekDay
+        
+        for (index, label) in buttonArray.enumerated() {
+            if index < showModel.manager.date.monthDay() {
+                label.alpha = 1
+            } else {
+                label.alpha = 0;
+            }
+        }
     }
     
     func clear() {
-        self.dateGroupView.clear()
+        selectedButton?.backgroundColor = UIColor.clear
+        selectedButton?.isSelected = false
     }
     
     func select(date:Int) {
-        self.dateGroupView.updateSelected(day: date)
+        updateSelected(day: date)
+    }
+    
+    @objc func click(button:UIButton) {
+        self.updateSelected(day: button.tag + 1)
     }
     
     override func layoutSubviews() {
         super.layoutSubviews()
         setupView()
+    }
+    
+    func updateSelected(day:Int) {
+        selectedButton?.backgroundColor = UIColor.clear
+        selectedButton?.isSelected = false
+        selectedButton = buttonArray[safe: day - 1]
+        selectedButton?.backgroundColor = UIColor.red
+        selectedButton?.isSelected = true
     }
     
     required init?(coder aDecoder: NSCoder) {
